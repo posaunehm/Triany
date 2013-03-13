b25defd658fd1e6c4c80d669c0636c16fd838703
posaunehm <posaunehm@gmail.com>
2013-03-13 12:37:30 +0900

    rootTrianyに対する動きを作成

--- ProjectTriany.Test/TrianyRepositoryTest.cs
+++ ProjectTriany.Test/TrianyRepositoryTest.cs
@@ -4,12 +4,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using ProjectTriany;
 
 namespace ProjectTriany.Test
 {
     [TestFixture]
     public class TrianyRepositoryTest
     {
-        
+        [TestCase]
+        public void ルートトリアニーのIDを取得する()
+        {
+            var sut = new TrianyRepository();
+
+            sut.RootTorianyId().Is(1u);
+        }
+            
+        [TestCase]
+        public void ルートトリアニーのAに値が設定できる()
+        {
+            var sut = new TrianyRepository();
+
+            var rootID = sut.RootTorianyId();
+
+            sut.SetA(rootID, 1u);
+            sut.GetA(rootID).Is(1u);
+        }
+
+
+        [TestCase]
+        public void ルートトリアニーのBに値が設定できる()
+        {
+            var sut = new TrianyRepository();
+
+            var rootID = sut.RootTorianyId();
+
+            sut.SetB(rootID, 1u);
+            sut.GetB(rootID).Is(1u);
+        }
+
+        [TestCase]
+        public void ルートトリアニーのCに値が設定できる()
+        {
+            var sut = new TrianyRepository();
+
+            var rootID = sut.RootTorianyId();
+
+            sut.SetC(rootID, 1u);
+            sut.GetC(rootID).Is(1u);
+        }
     }
 }
---
+++ ProjectTriany/TrianyRepository.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectTriany
+{
+    public class TrianyRepository
+    {
+        private const int ROOT_ID = 1;
+        Dictionary<uint, Triany> TrianyStore = new Dictionary<uint, Triany>();
+
+        public TrianyRepository()
+        {
+            TrianyStore.Add(ROOT_ID,new Triany());
+        }
+
+        public uint RootTorianyId()
+        {
+            return ROOT_ID;
+        }
+
+        public void SetA(uint id, uint u)
+        {
+            TrianyStore[id].A = u;
+        }
+
+        public uint GetA(uint id)
+        {
+            return TrianyStore[id].A;
+        }
+
+        public void SetB(uint id, uint u)
+        {
+            TrianyStore[id].B = u;
+        }
+
+        public uint GetB(uint id)
+        {
+            return TrianyStore[id].B;
+        }
+
+        public void SetC(uint id, uint u)
+        {
+            TrianyStore[id].C = u;
+        }
+
+        public uint GetC(uint id)
+        {
+            return TrianyStore[id].C;
+        }
+    }
+
+    internal class Triany
+    {
+        public uint A { get; set; }
+
+        public uint B { get; set; }
+
+        public uint C { get; set; }
+    }
+}
